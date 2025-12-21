6f19ba0990a923a9e8b8beb41045027f2f6bffdb
Yuri Martins <yurimn14@outlook.com>
2025-12-21 17:49:13 -0300

    Fix entity conflics with tables

--- src/main/java/com/example/demo/model/Endereco.java
+++ src/main/java/com/example/demo/model/Endereco.java
@@ -3,11 +3,13 @@ package com.example.demo.model;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Embeddable
 @AllArgsConstructor
+@NoArgsConstructor
 public class Endereco {
     private String logradouro;
     private String numero;
